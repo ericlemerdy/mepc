05eb054650a2c9bc9c59453ac1c1e66e5ba93f13
Eric Le Merdy <eric.lemerdy.ext@mappy.com>
2012-06-29 19:35:02 +0200

    Work in progress

--- mepc-jenkins/downloadCookbooks.sh
+++ mepc-jenkins/downloadCookbooks.sh
@@ -10,13 +10,13 @@ else
 	git clone https://github.com/opscode-cookbooks/ark.git
 	git clone https://github.com/opscode-cookbooks/git.git
 	git clone https://github.com/opscode-cookbooks/java.git
-        mkdir --verbose jenkins/
+	mkdir --verbose jenkins/
 	cd jenkins
 	wget https://gist.github.com/gists/978920/download --output-document=978920.tgz
 	tar --verbose --extract --file=978920.tgz
 	mv --verbose gist* recipes
 	rm --verbose 978920.tgz
-        cd ../
+	cd ../
 	git clone https://github.com/opscode-cookbooks/maven.git
 	cd ../
 fi
