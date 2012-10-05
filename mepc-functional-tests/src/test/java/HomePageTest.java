ec5495e5852e8ca2779a7c77d9dc3c394a31f3b1
Eric Le Merdy <eric.lemerdy@laposte.net>
2012-10-05 19:25:02 +0200

    [fonc. tests] Green test: static resources are served by jersey test server and jweb unit validate page.

--- mepc-functional-tests/src/test/java/HomePageTest.java
+++ mepc-functional-tests/src/test/java/HomePageTest.java
@@ -1,22 +1,71 @@
+import static java.lang.String.format;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+import net.gageot.test.rules.ServiceRule;
 import net.sourceforge.jwebunit.junit.WebTester;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 
+import com.google.inject.Module;
+import com.sun.jersey.api.container.httpserver.HttpServerFactory;
+import com.sun.jersey.api.core.DefaultResourceConfig;
+import com.sun.net.httpserver.HttpServer;
+
+@Path("/")
+@SuppressWarnings("restriction")
 public class HomePageTest {
+
+    @GET
+    public File index() {
+	return new File("../static/index.html");
+    }
+
+    @GET
+    @Path("/{subfolder}")
+    public File index(@PathParam("subfolder") String subFolder) {
+	return new File(format("../static/%s",
+		subFolder));
+    }
+
+    @Rule
+    public ServiceRule<HomePageTest> serviceRule = ServiceRule
+	    .startWithRandomPort(HomePageTest.class, (Module) null);
+
+    private HttpServer httpServer;
 
     private WebTester webTester;
 
     @Before
     public void createWebTester() {
 	webTester = new WebTester();
-	webTester.setBaseUrl("http://localhost:8080/");
+	webTester.setBaseUrl(format("http://localhost:%d/",
+		serviceRule.getPort()));
     }
 
     @Test
     public void should_display_hello_world_on_home_page() {
 	webTester.beginAt("/");
-	webTester.assertTextPresent("Hollo, world !");
+	webTester.assertTextPresent("Hello, world!");
+    }
+
+    public void start(int port) throws IllegalArgumentException,
+	    IOException {
+	httpServer = HttpServerFactory.create(format("http://localhost:%d/",
+ port),
+		new DefaultResourceConfig(HomePageTest.class));
+	httpServer.start();
+    }
+
+    public void stop() {
+	httpServer.stop(0);
     }
 
 }
