2ab2e5ff8f58e14e3e836dd4941021eacb08a3de
Eric Le Merdy <eric.lemerdy@laposte.net>
2012-11-27 22:23:20 +0100

    Externalize data and templatize client-side with Mustache.

--- mepc-functional-tests/src/test/java/HomePageTest.java
+++ mepc-functional-tests/src/test/java/HomePageTest.java
@@ -1,12 +1,16 @@
 import static java.lang.String.format;
 import lightweightTestServer.MepcStaticResourcesServer;
 import net.gageot.test.rules.ServiceRule;
+import net.sourceforge.jwebunit.htmlunit.HtmlUnitTestingEngineImpl;
 import net.sourceforge.jwebunit.junit.WebTester;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import com.gargoylesoftware.htmlunit.AjaxController;
+import com.gargoylesoftware.htmlunit.NicelyResynchronizingAjaxController;
+import com.gargoylesoftware.htmlunit.WebClient;
 import com.google.inject.Module;
 
 public class HomePageTest {
@@ -22,17 +26,29 @@ public class HomePageTest {
 		webTester = new WebTester();
 		webTester.setBaseUrl(format("http://localhost:%d/",
 				serviceRule.getPort()));
+		webTester.beginAt("/");
+		configureSynchronousAjax();
+	}
+
+	private void configureSynchronousAjax() {
+		if (webTester.getTestingEngine() instanceof HtmlUnitTestingEngineImpl) {
+			HtmlUnitTestingEngineImpl testingEngine = (HtmlUnitTestingEngineImpl) webTester
+					.getTestingEngine();
+			WebClient webClient = testingEngine.getWebClient();
+			AjaxController ajaxController = new NicelyResynchronizingAjaxController();
+			webClient.setAjaxController(ajaxController);
+		}
 	}
 
 	@Test
 	public void should_display_the_list_of_soldiers() {
-		webTester.beginAt("/");
+		webTester.gotoPage("/");
 		webTester.assertTextInElement("brand", "Soldier Store");
 	}
 
 	@Test
 	public void should_display_rambo() {
-		webTester.beginAt("/");
+		webTester.gotoPage("/");
 		webTester.assertTextPresent("Sylvester Stallone");
 	}
 
