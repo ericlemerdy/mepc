abc7c4f388fbf20c251fd408c77575da2aecbf06
root <root@lxc-host.(none)>
2012-12-18 00:29:54 +0100

    [CI] Add validate script and correct build_and_deploy

---
+++ scripts/ci/build_and_deploy.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+REPO_DIR=/var/repos/mepc
+PROJECT_DIR=${REPO_DIR}/java
+
+cd ${REPO_DIR}
+UPDATE=`/usr/bin/git fetch --dry-run 2>&1 |/usr/bin/wc -l`
+if [ $UPDATE -ne 0 || true ]
+then
+	/usr/bin/git pull --rebase
+	cd ${PROJECT_DIR}
+	/usr/bin/mvn clean install
+	if [ $? -eq 0 ]
+	then
+		/var/repos/mepc/scripts/lxc/launch_env.sh int
+		/var/repos/mepc/scripts/ci/deploy.sh int
+		/var/repos/mepc/scripts/ci/validate.sh int
+		if [ $? -eq 0 ]
+		then
+			EXISTING_PROD=`sudo lxc-list |grep -E '^  (blue|green)app' |sed 's/^ *\(.*\)app.*/\1/'`
+			if [ ${EXISTING_PROD} -eq 'blue' ]
+			then
+				TARGET_PROD=green
+			else
+				TARGET_PROD=blue
+			fi
+			/var/repos/mepc/scripts/lxc/launch_env.sh ${TARGET_PROD}
+			/var/repos/mepc/scripts/ci/deploy.sh ${TARGET_PROD}
+			/var/repos/mepc/scripts/ci/validate.sh ${TARGET_PROD}
+		else
+			echo Invalid deployment.
+		fi
+	else
+		echo Build failed.
+	fi
+else
+	echo No change found.
+fi
---
+++ scripts/ci/validate.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ENV_TYPE=$1
