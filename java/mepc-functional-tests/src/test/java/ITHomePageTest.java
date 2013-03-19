bf80ddf4331f696e533ec2a333f900310ee5d3f0
Eric Le Merdy <eric.lemerdy@laposte.net>
2013-03-19 09:03:16 +0100

    [java] replace jwebunit with fluentlenium

--- java/mepc-functional-tests/src/test/java/ITHomePageTest.java
+++ java/mepc-functional-tests/src/test/java/ITHomePageTest.java
@@ -1,49 +1,25 @@
-import net.sourceforge.jwebunit.htmlunit.HtmlUnitTestingEngineImpl;
-import net.sourceforge.jwebunit.junit.WebTester;
+import static org.fest.assertions.fluentlenium.FluentLeniumAssertions.assertThat;
 
-import org.junit.After;
+import org.fluentlenium.adapter.FluentTest;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.gargoylesoftware.htmlunit.NicelyResynchronizingAjaxController;
-import com.gargoylesoftware.htmlunit.WebClient;
-
-public class ITHomePageTest {
-
-	private WebTester webTester;
+public class ITHomePageTest extends FluentTest {
 
 	@Before
 	public void createWebTester() {
-		webTester = new WebTester();
-		webTester.setBaseUrl("http://localhost:8080/");
-		webTester.beginAt("/");
-		getWebClient().setAjaxController(new NicelyResynchronizingAjaxController());
-	}
-
-	private WebClient getWebClient() {
-		if (webTester.getTestingEngine() instanceof HtmlUnitTestingEngineImpl) {
-			final HtmlUnitTestingEngineImpl testingEngine = (HtmlUnitTestingEngineImpl) webTester.getTestingEngine();
-			return testingEngine.getWebClient();
-		}
-		return null;
+		goTo("http://localhost:8080/");
+		await().until(".soldier-name").isPresent();
 	}
 
 	@Test
 	public void should_display_the_app() {
-		webTester.gotoPage("/");
-		webTester.assertTextInElement("brand", "Soldier Store");
+		assertThat(findFirst(".brand")).hasText("SOLDIER STORE");
 	}
 
 	@Test
 	public void should_display_stallone() {
-		webTester.gotoPage("/");
-		getWebClient().waitForBackgroundJavaScript(2000);
-		webTester.assertTextPresent("Sylvester Stallone");
-	}
-
-	@After
-	public void close() {
-		webTester.closeBrowser();
+		assertThat(find(".soldier-name")).hasText("Sylvester Stallone");
 	}
 
 }
