e48c55a3a34a4ffbe1b25ab74366f3869a981c2d
Eric Le Merdy <eric.lemerdy@laposte.net>
2013-03-23 07:19:23 +0100

    [java] covering json rest api

--- java/mepc-functional-tests/src/test/java/ITDataTest.java
+++ java/mepc-functional-tests/src/test/java/ITDataTest.java
@@ -5,6 +5,7 @@ import static org.apache.http.HttpStatus.SC_FORBIDDEN;
 import static org.apache.http.HttpStatus.SC_OK;
 import static org.fest.assertions.Assertions.assertThat;
 import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
 import java.net.URL;
@@ -22,7 +23,7 @@ public class ITDataTest {
 
 	@Test
 	public void should_get_soldiers() throws Exception {
-		expect().body("soldiers.soldiers.id", contains( //
+		expect().content("soldiers.soldiers.id", contains( //
 				contains((Object) "stallone", "statham", "li"), //
 				contains((Object) "lundgren", "norris", "van-damme"), //
 				contains((Object) "willis", "schwarzenegger") //
@@ -37,6 +38,16 @@ public class ITDataTest {
 	}
 
 	@Test
+	public void soldier_should_have_some_attributes() throws Exception {
+		expect().root("soldiers.soldiers[0][0]")//
+				.content("id", equalTo("stallone")).and() //
+				.content("name", equalTo("Sylvester Stallone")) //
+				.content("description", equalTo("This ex-boxer is a vietnâm veteran that really had a rough.")).and() //
+				.content("hired", equalTo(false)).and() //
+				.when().get(format("http://%s/data/soldiers.json", getAppHost()));
+	}
+
+	@Test
 	public void should_not_hire_stallone_twice() throws Exception {
 		final String hireURL = format("http://%s/data/hire/stallone", getAppHost());
 		expect().statusCode(SC_OK).when().post(hireURL);
@@ -47,6 +58,7 @@ public class ITDataTest {
 	public void should_hire_statham() throws Exception {
 		final String hireURL = format("http://%s/data/hire/statham", getAppHost());
 		expect().statusCode(SC_OK).when().post(hireURL);
+		expect().body("soldiers.soldiers[0][1].hired", equalTo(true)).when().get("http://{appHost}/data/soldiers.json", getAppHost());
 	}
 
 	@Test
