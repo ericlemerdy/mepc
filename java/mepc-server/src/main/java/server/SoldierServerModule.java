8d4732b63436ba8a5e3b422c076c8ce86322d2cf
Eric Le Merdy <eric.lemerdy@laposte.net>
2013-03-27 07:06:49 +0100

    [java] read hibernate configuration from file

--- java/mepc-server/src/main/java/server/SoldierServerModule.java
+++ java/mepc-server/src/main/java/server/SoldierServerModule.java
@@ -1,5 +1,6 @@
 package server;
 
+import static com.google.common.io.Files.newReader;
 import static org.hibernate.cfg.Environment.DIALECT;
 import static org.hibernate.cfg.Environment.DRIVER;
 import static org.hibernate.cfg.Environment.HBM2DDL_AUTO;
@@ -8,6 +9,11 @@ import static org.hibernate.cfg.Environment.POOL_SIZE;
 import static org.hibernate.cfg.Environment.SHOW_SQL;
 import static org.hibernate.cfg.Environment.URL;
 import static org.hibernate.cfg.Environment.USER;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 
 import javax.inject.Singleton;
 
@@ -21,6 +27,7 @@ import org.hibernate.cfg.AnnotationConfiguration;
 import resources.Soldier;
 import resources.Soldiers;
 
+import com.google.common.base.Charsets;
 import com.google.common.collect.ImmutableMap;
 import com.google.inject.Provides;
 import com.sun.jersey.api.json.JSONConfiguration;
@@ -42,16 +49,30 @@ public class SoldierServerModule extends JerseyServletModule {
 
 	@Provides
 	@Singleton
-	public SessionFactory createSessionFactory() {
+	public SessionFactory createSessionFactory() throws FileNotFoundException, IOException {
+		Properties properties;
+
+		Properties defaultProperties = new Properties();
+		defaultProperties.put(DRIVER, "com.mysql.jdbc.Driver");
+		defaultProperties.put(URL, "jdbc:mysql://localhost/mepcdata");
+		defaultProperties.put(USER, "root");
+		defaultProperties.put(PASS, "root");
+		defaultProperties.put(POOL_SIZE, "1");
+		defaultProperties.put(DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+		defaultProperties.put(SHOW_SQL, "true");
+		defaultProperties.put(HBM2DDL_AUTO, "update");
+
+		try {
+			Properties propertiesFromFile = new Properties();
+			propertiesFromFile.load(newReader(new File("/etc/mepc/", "mepc-server.properties"), Charsets.UTF_8));
+			properties = propertiesFromFile;
+		} catch (IOException e) {
+			System.out.println("Default file not loaded. Using hard-coded defaults");
+			properties = defaultProperties;
+		}
+
 		return new AnnotationConfiguration() //
-				.setProperty(DRIVER, "com.mysql.jdbc.Driver") //
-				.setProperty(URL, "jdbc:mysql://localhost/mepcdata") //
-				.setProperty(USER, "root") //
-				.setProperty(PASS, "root") //
-				.setProperty(POOL_SIZE, "1") //
-				.setProperty(DIALECT, "org.hibernate.dialect.MySQL5Dialect") //
-				.setProperty(SHOW_SQL, "true") //
-				.setProperty(HBM2DDL_AUTO, "update") //
+				.addProperties(properties) //
 				.addAnnotatedClass(resources.Soldier.class) //
 				.buildSessionFactory();
 	}
